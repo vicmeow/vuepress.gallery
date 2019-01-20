94c1c0083d84b3cc7eab2ad9d203eb3edb1d1834
vicbergquist <victoria.bergquist@gmail.com>
2019-01-20 18:35:31 +0100

    maintenance

--- showcase/.vuepress/theme/Layout.vue
+++ showcase/.vuepress/theme/Layout.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="wrapper">
+    <!-- TODO: Make header component -->
     <h1 class="page-title">{{ $page.frontmatter.title }}</h1>
     <p class="page-description">A curated list of custom <a href="https://vuepress.vuejs.org" rel="noopener">VuePress</a> themes.</p>
     <component :is="layout"></component>
+    <!-- TODO: Make footer component -->
   </div>
 </template>
 <script>
