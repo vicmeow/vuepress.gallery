6698628d740c341596b641af52fa0e58ed31a51e
vicbergquist <victoria.bergquist@gmail.com>
2019-01-20 15:20:56 +0100

    gradient overlay, temp removed title link

--- .vuepress/theme/layouts/ListLayout.vue
+++ .vuepress/theme/layouts/ListLayout.vue
@@ -1,13 +1,15 @@
 <template>
   <ul class="list-layout">
     <li class="list-item" v-for="site in sites" :key="site.index">
+      <div class="gradient-overlay"></div>
     <figure class="figure">
       <img class="site-img" :src="site.img" :alt="site.title + ' screenshot'">
-      <figcaption class="caption">
+      <!-- TODO: Include name, tags? Must have link to website OR repo -->
+      <!-- <figcaption class="caption">
         <a :href="site.url">
           <h2 class="site-title">{{ site.title }}</h2>
         </a>
-      </figcaption>
+      </figcaption> -->
     </figure>
     </li>
   </ul>
@@ -23,7 +25,7 @@
         'tags': ['blog'],
         'url': 'https://bencodezen.io',
         'likes': 12
-    },
+        },
         {
         'title': 'vuepress-portfolio',
         'img': 'acme.jpg',
@@ -70,35 +72,55 @@
 .list-layout
   min-height: 100vh
   display: grid
-  grid-template-columns: repeat(auto-fill, minmax(430px, 1fr))
+  grid-template-columns: repeat(auto-fill, minmax(330px, 1fr))
   gap: 1em
   margin: 0 auto
   justify-content: center
-  padding: 1em 2em
+  padding: 2em 2em
   list-style-type: none
   margin: 0
 
 .list-item
-  max-height: 400px
-  box-shadow: 0px 2px 10px rgba(0,0,0,.1)
-  padding: 1em
-  border-radius: 1em
+  position: relative
+  height: 430px
+  box-shadow: 0px 2px 15px rgba(0,0,0,.1)
+  border-radius: .5em
+  background: white
+  overflow: hidden
+  padding: 1em .5em
+  transition: all .5s ease-in-out
+  &:hover
+    transform: scale(1.02)
 
 .figure
   height: 100%
-  display: grid
-  grid-template-rows: 1fr max-content
+  //display: grid
+  //grid-template-rows: 1fr max-content
   margin: 0
+
+.caption
+  padding: .5em 1em
+  display: flex
+  justify-content: space-between
 
 .site-img
   height: 100%
   width: 100%
   object-fit: cover
+  object-position: 0 0
 
 .site-title
   font-size: 1em
-  margin-top: .5em
 
+.likes
+  opacity: .5
 
+.gradient-overlay
+  position: absolute
+  top: 0
+  right: 0
+  bottom: 0
+  left: 0
+  background: linear-gradient(rgba(255,255,255,0) 80%, #FFFFFF 100%) no-repeat 50% 50% / 100% 100%
 
 </style>
