9c6744328ee3099fc56e5dbdab8f081f62752397
vicbergquist <victoria.bergquist@gmail.com>
2019-02-03 22:51:23 +0100

    config update

--- gallery/.vuepress/config.js
+++ gallery/.vuepress/config.js
@@ -3,15 +3,20 @@ module.exports = {
   description: 'A VuePress theme gallery, made with VuePress.',
   serviceWorker: true,
   plugins: [
-    [ 
+    [
       '@vuepress/google-analytics',
       {
-        'ga': 'UA-133690351-1'
-      }
+        ga: 'UA-133690351-1'
+      }	
     ],
     '@vuepress/plugin-pwa'
   ],
   head: [
-    ['meta', { name: 'google-site-verification', content: 'nvnApLC80g7AZLICqtOpuMvdcU-CjQ7gJsku-gpkZAc'}]
+    ['meta', { name: 'google-site-verification', content: 'nvnApLC80g7AZLICqtOpuMvdcU-CjQ7gJsku-gpkZAc'}],
+    ['meta', { name: 'twitter:card', content: 'summary'}],
+    ['meta', { name: 'twitter:title', content: 'VuePress Gallery'}],
+    ['meta', { name: 'twitter:creator', content: '@vicbergquist'}],
+    ['meta', { name: 'twitter:site', content: '@vicbergquist'}],
+    ['meta', { name: 'twitter:description', content: 'A VuePress theme gallery, made with VuePress.'}]
   ]
 }
