67ef16b3f44efb5d35370032fc4611462d297029
vicbergquist <victoria.bergquist@gmail.com>
2019-02-03 23:34:03 +0100

    twitter img

--- gallery/.vuepress/config.js
+++ gallery/.vuepress/config.js
@@ -14,7 +14,8 @@ module.exports = {
   head: [
     ['meta', { name: 'google-site-verification', content: 'nvnApLC80g7AZLICqtOpuMvdcU-CjQ7gJsku-gpkZAc'}],
     ['meta', { name: 'twitter:card', content: 'summary'}],
-    ['meta', { name: 'twitter:title', content: 'VuePress Gallery'}],
+    ['meta', { name: 'twitter:title', content: 'VuePress Gallery 💚'}],
+    ['meta', { name: 'twitter:image', content: 'https://vuepress.gallery/twitter-card.png'}],
     ['meta', { name: 'twitter:creator', content: '@vicbergquist'}],
     ['meta', { name: 'twitter:site', content: '@vicbergquist'}],
     ['meta', { name: 'twitter:description', content: 'A VuePress theme gallery, made with VuePress.'}]
