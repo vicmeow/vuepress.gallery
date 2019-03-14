6d78a3dfa8e93679007cf98c2181332359ad304a
vicbergquist <victoria.bergquist@gmail.com>
2019-03-14 18:58:48 +0100

    installed, updated plugins

--- gallery/.vuepress/config.js
+++ gallery/.vuepress/config.js
@@ -10,39 +10,51 @@ module.exports = {
       // { text: 'Resources', link: '/resources/' },
       // { text: 'Components', link: '/library/' },
       { text: 'About', link: '/about/' },
-      { text: 'Submit', link: '/submit/' }
+      { text: 'Submit', link: '/submit/' },
       // { text: 'Submissions', link: 'https://github.com/vicbergquist/vuepress.gallery' }
-    ]
+    ],
   },
   markdown: {
     anchor: {
-      permalink: false
-    }
+      permalink: false,
+    },
   },
-  plugins: [
-    [
-      '@vuepress/google-analytics',
-      {
-        ga: 'UA-133690351-1'
-      }	
-    ]
-    // ['@vuepress/pwa',
-    // { 
-    //   serviceWorker: true
-    // }]
-  ],
+  plugins: {
+    '@vuepress/google-analytics': {
+      ga: 'UA-133690351-1',
+    },
+    '@vuepress/plugin-clean-urls': {
+      normalSuffix: '',
+    },
+  },
   head: [
-    ['meta', { name: 'google-site-verification', content: 'nvnApLC80g7AZLICqtOpuMvdcU-CjQ7gJsku-gpkZAc'}],
-    ['meta', { name: 'twitter:card', content: 'summary'}],
-    ['meta', { name: 'twitter:title', content: 'VuePress Gallery 💚'}],
-    ['meta', { name: 'twitter:image', content: 'https://vuepress.gallery/twitter-card.png'}],
-    ['meta', { name: 'twitter:creator', content: '@vicbergquist'}],
-    ['meta', { name: 'twitter:site', content: '@vicbergquist'}],
-    ['meta', { name: 'twitter:description', content: 'A VuePress theme gallery, made with VuePress.'}]
+    [
+      'meta',
+      {
+        name: 'google-site-verification',
+        content: 'nvnApLC80g7AZLICqtOpuMvdcU-CjQ7gJsku-gpkZAc',
+      },
+    ],
+    ['meta', { name: 'twitter:card', content: 'summary' }],
+    ['meta', { name: 'twitter:title', content: 'VuePress Gallery 💚' }],
+    [
+      'meta',
+      {
+        name: 'twitter:image',
+        content: 'https://vuepress.gallery/twitter-card.png',
+      },
+    ],
+    ['meta', { name: 'twitter:creator', content: '@vicbergquist' }],
+    ['meta', { name: 'twitter:site', content: '@vicbergquist' }],
+    [
+      'meta',
+      {
+        name: 'twitter:description',
+        content: 'A VuePress theme gallery, made with VuePress.',
+      },
+    ],
   ],
-  configureWebpack: (config) => {
-    return { plugins: [
-      new webpack.EnvironmentPlugin({ ...process.env })
-    ]}
-  }
+  configureWebpack: config => {
+    return { plugins: [new webpack.EnvironmentPlugin({ ...process.env })] }
+  },
 }
